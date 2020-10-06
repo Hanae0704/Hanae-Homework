3e6f0e666a8edae3544ff4ddfa0a3679bea0c279
Hanae Izumiguchi <izumiguchihanae@MacBook-Pro.local>
2020-10-05 17:14:08 -0700

    hm1

--- src/React/Main.jsx
+++ src/React/Main.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
 /* Components ---------------------------*/
-import Test from './Test.jsx';
+
 
 const Main = () => {
 
     return (
         <div className='Main'>
-            <Test />
+            <h1>Homework</h1>
+
+            <h2>Essay 1</h2>
+            <p>- What are Single Page Apps (SPAs)? How do they differ from traditional multi-page Web Sites?</p>
+
+            <p>Single page app differs from traditional multipage website in a way that SPA does not involved navigating to an entirely new page, while traditional multipage website navigates to a new page. 
+<br></br>In other words, SPA pages load within the same page 
+while traditional multipage websites do not and go to an entirely new page. </p>
         </div>
     );
 }
 
 export default Main;
+
